bb04635b58aab5da0745308109565130c0720426
Audi23zx-23 <enmanuel.lopez-m@mail.escuelaing.edu.co>
2026-02-02 07:45:26 -0500

    Update ejercicio01.pl

--- LAB01/ejecicio01/ejercicio01.pl
+++ LAB01/ejecicio01/ejercicio01.pl
@@ -1,1 +1,21 @@
+% Hechos: relación entre ciudades
+ciudad(bogota).
+ciudad(medellin).
+ciudad(cali).
+ciudad(cartagena).
+ciudad(manizales).
+ciudad(barranquilla).
+ciudad(pasto).
+ciudad(monteria).
 
+% Hechos: vuelos directos
+vuelo(bogota, medellin).
+vuelo(medellin, cartagena).
+vuelo(cali, bogota).
+vuelo(bogota, cartagena).
+vuelo(manizales, cartagena).
+vuelo(medellin, barranquilla).
+vuelo(pasto, bogota).
+vuelo(bogota, pasto).
+
+vuelo_directo(X, Y) :- (vuelo(X, Y), ciudad(X), ciudad(Y)).
